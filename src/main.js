f4166f4eb546aa43116d987d36a3a05efaccb49d
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2024-10-23 22:47:35 -0500

    Pinia initialization

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-//import { createPinia } from 'pinia'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from "./router.js";
 import vuetify from "./plugins/vuetify.js";
 
-//createApp(App).use(router).use(createPinia()).use(vuetify).mount('#app');
-createApp(App).use(router).use(vuetify).mount('#app');
+createApp(App).use(router).use(createPinia()).use(vuetify).mount('#app');
+//createApp(App).use(router).use(vuetify).mount('#app');
