5e90e18e7310a2eebf95ea9b8622c9a5f10974e0
AnthonyPhamOC <97481024+AnthonyPhamOC@users.noreply.github.com>
2024-10-25 09:20:44 -0500

    Feature (Frontend) Added Basic routing to other pages involving resumes.

--- src/App.vue
+++ src/App.vue
@@ -4,44 +4,35 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <nav>
+      <ul>
+        <div>
+          <li>
+            <a>{{ "Resume Listing" }}</a>
+          </li>
+        </div>
+      </ul>
+      <ul>
+        <div>
+          <li>
+            <router-link :to="{name: 'resumes'}"><a>LIST</a></router-link>
+          </li>
+          <li>
+            <router-link :to="{name: 'resume', params: { id: 1 }}"><a>VIEW RESUME</a></router-link>
+          </li>
+          <li>
+            <router-link :to="{name: 'addResume'}"><a>ADD RESUME</a></router-link>
+          </li>
+          <li>
+            <router-link :to="{name: 'editResume', params: { id: 1 }}"><a>EDIT RESUME</a></router-link>
+          </li>
+          <li>
+            <router-link :to="{name: 'profile'}"><a>VIEW PROFILE</a></router-link>
+          </li>
+        </div>
+      </ul>
+    </nav>
+    <router-view />
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
+import ListResume from "./views/ListResume.vue";
+import ViewResume from "./views/ViewResume.vue";
+import AddResume from "./views/AddResume.vue";
+import EditResume from "./views/EditResume.vue";
+import ViewProfile from "./views/ViewProfile.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+    {
+      path: "/",
+      name: "resumes",
+      component: ListResume,
+    },
+    {
+      path: "/resume/:id",
+      name: "resume",
+      component: ViewResume,
+      props: true,
+    },
+    {
+      path: "/add",
+      name: "addResume",
+      component: AddResume,
+    },
+    {
+      path: "/edit/:id",
+      name: "editResume",
+      component: EditResume,
+      props: true,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ViewProfile,
+    },
   ],
 });
 
