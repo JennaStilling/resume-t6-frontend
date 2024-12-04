e6d6855f1247177784819dcd305a9c0ca149f96c
HeyBail <Bill331333@gmail.com>
2024-12-03 18:38:03 -0600

    fix (cohere) cohere secrete hopfully accessable

--- src/services/cohereClientServices.js
+++ src/services/cohereClientServices.js
@@ -1,7 +1,6 @@
 import { CohereClient } from "cohere-ai";
 
 const apiKey = import.meta.env.VITE_APP_COHERE_API_KEY;
-console.log("Cohere API Key:", apiKey);
 
 export const cohereClient = new CohereClient({
   token: import.meta.env.VITE_APP_COHERE_API_KEY,
